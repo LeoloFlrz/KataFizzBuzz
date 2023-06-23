43705dbf3adaa3774e0e365c3e0e58e758ea33de
Leolo Flórez <116220158+LeoloFlrz@users.noreply.github.com>
2023-06-23 10:04:50 +0200

    function createArray fixed

--- src/scripts/FizzBuzz.js
+++ src/scripts/FizzBuzz.js
@@ -1,40 +1,23 @@
 function createArray() {
   const numbers = [];
+
   for (let i = 0; i < 100; i++) {
     numbers[i] = i + 1;
+    
+      if (numbers[i] % 3 == 0 && numbers[i] % 5 == 0) {
+        numbers[i] = "FizzBuzz";
+      } else if (numbers[i] % 3 == 0) {
+        numbers[i] = "Fizz";
+      } else if (numbers[i] % 5 == 0) {
+        numbers[i] = "Buzz";
+      }else {
+        numbers[i];
+      }
+    
   }
+  console.log(numbers);
   return numbers;
 }
+  
+createArray()
 
-const numero = createArray();
-
-function convertirFizzBuzz(numero) {
-  for (let i = 0; i < numero.length; i++) {
-    if(numero % 3 == 0 && numero % 5 == 0) {
-    numero = "FizzBuzz"
-  } else if (numero % 3 == 0) {
-    numero = "Fizz"
-  } else if (numero % 5 == 0) {
-    numero = "Buzz"
-  } 
-  return numero;
-  }
-}
-
-// function convertFizzBuzz(numero){
-//   let result;
-//   if (numero % 3 == 0 && numero % 5 == 0) {
-//       return result = "FizzBuzz";
-//   } else if (numero % 3 == 0) {
-//       return result = "Fizz";
-//   } else if (numero % 5 == 0) {
-//       return result = "Buzz";
-//   } else {
-//       return numero;
-//   }
-// }
-
-
-
-console.log(createArray());
-console.log(convertirFizzBuzz(numero));
